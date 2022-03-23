6b0bcf11e2ac7a92e890de9341ff339e9af41bf7
dylanb-101 <dylanbarrett360@gmail.com>
2022-03-22 22:04:25 -0400

    bca days of school

--- commands/schoolLeftBCA.js
+++ commands/schoolLeftBCA.js
@@ -2,14 +2,14 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('dos')
+		.setName('dosbca')
 		.setDescription('Tells you how many days of school are left'),
 	async execute(interaction) {
         let date = new Date();
         let today = new Date(`${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}`)
-        let endOfSchool = new Date('6/16/22');
+        let endOfSchool = new Date('6/25/22');
         let DIT = endOfSchool.getTime() - today.getTime();
         let DID = DIT / (1000 * 3600 * 24) + 1;
-		await interaction.reply({ content: `There are ${DID} days of school left`, ephemeral: true});
+		await interaction.reply({ content: `There are ${DID} days of school left in BCA`, ephemeral: false});
 	},
 };
---
+++ commands/schoolLeftHills.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('doshills')
+		.setDescription('Tells you how many days of school are left'),
+	async execute(interaction) {
+        let date = new Date();
+        let today = new Date(`${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}`)
+        let endOfSchool = new Date('6/16/22');
+        let DIT = endOfSchool.getTime() - today.getTime();
+        let DID = DIT / (1000 * 3600 * 24) + 1;
+		await interaction.reply({ content: `There are ${DID} days of school left in hills`, ephemeral: true});
+	},
+};
