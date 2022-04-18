1953db814e6643260ee806fa05e3c1902b355c33
dylanb-101 <dylanbarrett360@gmail.com>
2022-04-17 22:21:14 -0400

    status command changed

--- commands/status.js
+++ commands/status.js
@@ -22,7 +22,7 @@ module.exports = {
 		if (interaction.user.id === '511198318009909259' ||'741343008145801307' || '611372334498054174' || '543508185785303042') {
       client.user.setActivity(interaction.options.getString("status"), { type: interaction.options.getString("type") });
       
-      await interaction.reply({ content: 'Set the status to: ' + interaction.options.getString('type') + ' ' + interaction.options.getString("status"), ephemeral: false});
+      await interaction.reply({ content: `Set the status to: ${interaction.options.getString('type')} ${interaction.options.getString("status")}`, ephemeral: false});
     } else {
       await interaction.reply({ content: 'Did you really think I would let anyone set the status :clown:', ephemeral: true});
     }
